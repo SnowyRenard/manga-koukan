50ee4f675fac3a7b7b34f0d3f9b65c6ffd3823c7
Snowy Renard <snowyrenard@gmail.com>
2025-06-09 21:27:17 +0200

    Fixed page spliting

--- src/archive/mod.rs
+++ src/archive/mod.rs
@@ -1,4 +1,4 @@
-use std::{fs::File, io::Cursor, path::PathBuf, sync::Mutex};
+use std::{cmp::Ordering, fs::File, io::Cursor, path::PathBuf};
 
 use image::{DynamicImage, GenericImage, GenericImageView, ImageReader};
 
@@ -20,61 +20,105 @@ pub trait Archive: Send {
     fn finish(self);
 }
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
+pub(crate) enum Side {
+    None,
+    Right,
+    Left,
+}
+#[derive(PartialEq)]
+pub struct Page {
+    pub image: DynamicImage,
+    pub(crate) index: usize,
+    pub(crate) side: Side,
+}
+impl From<&PathBuf> for Page {
+    fn from(path: &PathBuf) -> Self {
+        let mut index = path.to_string_lossy().into_owned();
+        index.retain(|c| "0123456789".contains(c));
+
+        Self {
+            image: ImageReader::open(path).unwrap().decode().unwrap(),
+            index: index.parse().unwrap_or_default(),
+            side: Side::None,
+        }
+    }
+}
+impl Eq for Page {}
+impl Ord for Page {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self.index.cmp(&other.index) {
+            Ordering::Less => Ordering::Less,
+            Ordering::Equal => self.side.cmp(&other.side),
+            Ordering::Greater => Ordering::Greater,
+        }
+    }
+}
+impl PartialOrd for Page {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.index.partial_cmp(&other.index).map(|cmp| match cmp {
+            Ordering::Less => Ordering::Less,
+            Ordering::Equal => self
+                .side
+                .partial_cmp(&other.side)
+                .unwrap_or(Ordering::Equal),
+            Ordering::Greater => Ordering::Greater,
+        })
+    }
+}
+
 pub(crate) mod tar;
 pub(crate) mod zip;
 
-pub(crate) fn create_page(config: &Config, archive: &Mutex<impl Archive>, entry: &PathBuf) {
-    match config.image_format {
-        Some(_) => convert_page(config, archive, entry),
-        None => load_page(archive, entry),
-    }
-}
-
-pub(crate) fn create_page_with_name(
-    config: &Config,
-    archive: &Mutex<impl Archive>,
-    entry: &PathBuf,
-    name: &str,
-) {
-    match config.image_format {
-        Some(_) => convert_page_with_name(config, archive, entry, name),
-        None => load_page_with_name(archive, entry, name),
-    }
-}
-
-fn load_page(archive: &Mutex<impl Archive>, entry: &PathBuf) {
-    load_page_with_name(archive, entry, entry.to_str().unwrap())
-}
-
-fn load_page_with_name(archive: &Mutex<impl Archive>, entry: &PathBuf, name: &str) {
-    // Write file from dir to archive
-    archive
-        .lock()
-        .unwrap()
-        .write_file(&mut File::open(entry.as_path()).unwrap(), name);
-}
-
-fn convert_page(config: &Config, archive: &Mutex<impl Archive>, entry: &PathBuf) {
-    let name = entry.file_stem().unwrap().to_str().unwrap();
-    create_page_with_name(config, archive, entry, &name)
-}
-
-fn convert_page_with_name(
-    config: &Config,
-    archive: &Mutex<impl Archive>,
-    entry: &PathBuf,
-    name: &str,
-) {
-    // Var prep
-    let mut image = ImageReader::open(entry).unwrap().decode().unwrap();
-    let format = config.image_format.unwrap();
+pub(crate) fn convert_page(config: &Config, entry: &PathBuf) -> Vec<Page> {
+    let mut pages = split_pages(entry.into(), config);
 
     // Remove the margins on the image
     if let Some(margin) = config.remove_margin {
-        process_margin(&mut image, margin);
+        pages
+            .iter_mut()
+            .for_each(|page| process_margin(&mut page.image, margin));
     }
 
-    // Resize the image to the final resolution
+    pages
+        .iter_mut()
+        .for_each(|page| resize(&mut page.image, config));
+
+    pages
+}
+
+fn process_margin(image: &mut DynamicImage, margin: f32) {
+    let mut left_margin = image.width();
+    let mut right_margin = 0;
+
+    let margin_color = &image.get_pixel(image.width() - 1, 0);
+
+    image
+        .to_rgba8()
+        .enumerate_pixels()
+        .for_each(|(x, _, pixel)| {
+            // Left
+            if x < left_margin && pixel != margin_color {
+                left_margin = x;
+            }
+            // Right
+            if x > right_margin && pixel != margin_color {
+                right_margin = x;
+            }
+        });
+
+    let offset = left_margin;
+    let width = right_margin - left_margin;
+
+    if margin <= offset as f32 / image.width() as f32 {
+        *image = image
+            .sub_image(offset, 0, width, image.height())
+            .to_image()
+            .into();
+    }
+}
+
+fn resize(image: &mut DynamicImage, config: &Config) {
     if let Some([mut w, mut h]) = config.resolution {
         if w != 0 || h != 0 {
             if w == 0 {
@@ -83,82 +127,33 @@ fn convert_page_with_name(
             if h == 0 {
                 h = (image.height() as f32 * (w as f32 / image.width() as f32)) as u32;
             }
-            image = image.resize(w, h, image::imageops::FilterType::Lanczos3);
+            *image = image.resize(w, h, image::imageops::FilterType::Lanczos3);
         }
-    }
-
-    // Change the extension of the file to the new image format
-    if config.split_pages && image.width() > image.height() {
-        split_pages(&mut image, archive, name, format);
-    } else {
-        // Write the data to the archive
-        let file_name = format!("{}.{}", name, format.extensions_str()[0]);
-        archive
-            .lock()
-            .unwrap()
-            .write_image(&image, format, &file_name);
     }
 }
 
-fn process_margin(image: &mut DynamicImage, margin: f32) {
-    if image.width() > image.height() {
-        let mut left_margin = image.width();
-        let mut right_margin = 0;
-
-        let margin_color = &image.get_pixel(image.width() - 1, 0);
-
-        image
-            .to_rgba8()
-            .enumerate_pixels()
-            .for_each(|(x, _, pixel)| {
-                // Left
-                if x < left_margin && pixel != margin_color {
-                    left_margin = x;
-                }
-                // Right
-                if x > right_margin && pixel != margin_color {
-                    right_margin = x;
-                }
-            });
-
-        let offset = left_margin;
-        let width = right_margin - left_margin;
-
-        if margin <= offset as f32 / image.width() as f32 {
-            *image = image
-                .sub_image(offset, 0, width, image.height())
+fn split_pages(page: Page, config: &Config) -> Vec<Page> {
+    if config.split_pages && page.image.width() > page.image.height() {
+        let mut image = page.image;
+        let page_a = Page {
+            image: image
+                .sub_image(0, 0, image.width() / 2, image.height())
                 .to_image()
-                .into();
-        }
+                .into(),
+            index: page.index,
+            side: Side::Left,
+        };
+        let page_b = Page {
+            image: image
+                .sub_image(image.width() / 2, 0, image.width() / 2, image.height())
+                .to_image()
+                .into(),
+            index: page.index,
+            side: Side::Right,
+        };
+
+        vec![page_a, page_b]
+    } else {
+        vec![page]
     }
-}
-
-fn split_pages(
-    image: &mut DynamicImage,
-    archive: &Mutex<impl Archive>,
-    name: &str,
-    format: image::ImageFormat,
-) {
-    let image_a = image
-        .sub_image(0, 0, image.width() / 2, image.height())
-        .to_image();
-    let image_b = image
-        .sub_image(
-            (image.width() / 2) + 1,
-            0,
-            image.width() / 2,
-            image.height(),
-        )
-        .to_image();
-
-    archive.lock().unwrap().write_image(
-        &image_a.into(),
-        format,
-        &format!("{}a.{}", name, format.extensions_str()[0]),
-    );
-    archive.lock().unwrap().write_image(
-        &image_b.into(),
-        format,
-        &format!("{}b.{}", name, format.extensions_str()[0]),
-    );
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
-use std::{path::PathBuf, sync::Mutex};
+use std::path::PathBuf;
 
-use archive::{create_page, create_page_with_name, tar::Tar, zip::Zip, Archive};
+use archive::{tar::Tar, zip::Zip, Archive};
 use rayon::prelude::*;
 
 use config::{ArchiveFormat, Config};
@@ -10,32 +10,63 @@ pub mod config;
 
 pub fn run(config: &Config) {
     // Get all the files recursively
-    let entries = std::fs::read_dir(&config.input)
+    let mut entries: Vec<PathBuf> = std::fs::read_dir(&config.input)
         .unwrap()
-        .filter_map(|res| res.map(|e| e.path()).ok())
+        .filter_map(|res| res.map(|entry| entry.path()).ok())
+        // Check if the entry is a valid image.
+        .filter(|entry| image::ImageFormat::from_path(&entry).is_ok())
         .collect();
+
+    // entries.sort();
+    entries.sort_by(|a, b| path_to_num(a).cmp(&path_to_num(b)));
 
     // Create archive
     match config.archive_format {
         ArchiveFormat::CBZ => {
-            let archive = Mutex::new(Zip::new(&config.output));
-            compress(config, &archive, &entries);
-            archive.into_inner().unwrap().finish();
+            let mut archive = Zip::new(&config.output);
+            compress(config, &mut archive, &entries);
+            archive.finish();
         }
         ArchiveFormat::CBT => {
-            let archive = Mutex::new(Tar::new(&config.output));
-            compress(config, &archive, &entries);
-            archive.into_inner().unwrap().finish();
+            let mut archive = Tar::new(&config.output);
+            compress(config, &mut archive, &entries);
+            archive.finish();
         }
     };
 }
+fn path_to_num(path: &PathBuf) -> usize {
+    let mut string = path.to_string_lossy().into_owned();
 
-fn compress(config: &Config, archive: &Mutex<impl Archive>, entries: &Vec<PathBuf>) {
-    // Convert all the files
-    entries.par_iter().for_each(|e| {
-        create_page(config, archive, e);
-    });
+    string.retain(|c| "0123456789".contains(c));
 
-    // Generate cover image
-    create_page_with_name(config, archive, &entries[0], "cover");
+    string.parse().unwrap()
+}
+
+fn compress(config: &Config, archive: &mut impl Archive, entries: &Vec<PathBuf>) {
+    if let Some(format) = config.image_format {
+        // Convert all the files
+        let mut pages = entries
+            .par_iter()
+            .map(|e| archive::convert_page(config, e))
+            .flatten()
+            .collect::<Vec<_>>();
+
+        pages.sort();
+
+        pages.iter().enumerate().for_each(|(index, page)| {
+            archive.write_image(
+                &page.image,
+                format,
+                &format!("{}.{}", index, format.extensions_str()[0]),
+            )
+        });
+
+        // Generate cover image
+        archive.write_image(
+            &archive::convert_page(config, &entries[0])[0].image,
+            format,
+            "cover",
+        );
+    } else {
+    }
 }
